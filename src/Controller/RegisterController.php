0aee9a831c1c6f0bc074b6290efee1815e35aaa5
Léo Grambert <leogrambert@protonmail.com>
2023-04-12 16:12:49 +0200

    feat: add login form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,17 +4,45 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(PersistenceManagerRegistry $doctrine)
+    {
+        $this->entityManager = $doctrine->getManager();
+    }
+
     #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            // WARNING https://symfony.com/doc/current/security.html
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($hashedPassword);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            // return $this->redirectToRoute('app_login');
+        }
 
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -7,9 +7,13 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Email;
 
 class RegisterType extends AbstractType
 {
@@ -18,19 +22,87 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'attr' => ['placeholder' => 'John'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a firstname',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your firstname should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
             ->add('lastname', TextType::class, [
                 'attr' => ['placeholder' => 'Doe'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a lastname',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your lastname should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
             ->add('email', EmailType::class, [
-                'attr' => ['placeholder' => 'john.doe@protonmail.com']
+                'attr' => ['placeholder' => 'john.doe@protonmail.com'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an email',
+                    ]),
+                    new Email([
+                        'message' => 'The email "{{ value }}" is not a valid email.',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your email should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
-            ->add('password', PasswordType::class, [
-                'attr' => ['placeholder' => '*********']
-            ])
-            ->add('confirm_password', PasswordType::class, [
-                'mapped' => false,
-                'attr' => ['placeholder' => '*********']
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => [
+                    'label' => 'Password', 
+                    'attr' => [
+                        'placeholder' => '*********'
+                    ], 
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 3,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password', 
+                    'attr' => [
+                        'placeholder' => '*********'
+                    ], 
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please repeat your password',
+                        ]),
+                        new Length([
+                            'min' => 3,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ]
+                ]
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Register',
