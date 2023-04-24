c842706a9e23180c6e3ee3f1d3f8220d6c17502a
Léo Grambert <leogrambert@protonmail.com>
2023-04-24 23:32:40 +0200

    feat: clean layout

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -22,26 +22,30 @@ class AccountPasswordController extends AbstractController
     #[Route('/account/password', name: 'app_account_password')]
     public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $notification = null;
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
 
         $form->handleRequest($request);
 
+        // WARNING https://symfony.com/doc/current/security.html
         if ($form->isSubmitted() && $form->isValid()) {
             $plainPassword = $form->get('new_password')->getData();
             $encodedPassword = $passwordHasher->hashPassword($user, $plainPassword);
             $user->setPassword($encodedPassword);
 
-            $this->entityManager->persist($user);
             $this->entityManager->flush();
 
-            $this->addFlash('success', 'Password changed!');
+            $notification = 'Password changed!';
 
             return $this->redirectToRoute('app_home');
+        } else if ($form->isSubmitted() && !$form->isValid()) {
+            $notification = 'Password not changed!';
         }
 
         return $this->render('account/password.html.twig', [
             'form' => $form->createView(),
+            'notification' => $notification,
         ]);
     }
 }
